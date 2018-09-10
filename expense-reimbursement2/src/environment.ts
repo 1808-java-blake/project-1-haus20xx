10e41b28799a4e03f62f80013e0df5949404a793
haus20xx <kennysmod@gmail.com>
2018-09-10 01:05:06 -0400

    making sure things are saved

--- expense-reimbursement2/src/environment.ts
+++ expense-reimbursement2/src/environment.ts
@@ -3,7 +3,7 @@ const dev = {
 }
 
 // const prod = {
-//   context: 'http://ec2-18-237-118-203.us-west-2.compute.amazonaws.com:3000'
+//   context: 'http://ec2-52-15-87-153.us-east-2.compute.amazonaws.com:3000'
 // }
 
 export const environment = dev.context;
